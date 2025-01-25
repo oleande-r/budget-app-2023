35da5b9307feef0687a9222cdd1547005cd6373c
oleande-r <25854525+oleande-r@users.noreply.github.com>
2025-01-25 04:37:09 -0600

    added a lambda function - updated docstrings - edited config to contain encrypt key - edited lambda functions to read key fromo config

--- lambda/create_budget_cat_function.py
+++ lambda/create_budget_cat_function.py
@@ -1,6 +1,6 @@
-"""Handles the event that a `POST: /auth` request is received.
+"""Handles the event that a `POST: /create/budget-category` request is received.
 
-This authenticates a user attempting to login.
+This creates a specified budget category for the user.
 """
 
 import json
@@ -11,7 +11,7 @@ from utils import datatier, auth, api_utils
 
 
 def lambda_handler(event, context):
-    """Authenticates a user attempting to login to the app.
+    """Creates a new budget category for the current user.
 
     Args:
         event (dict): A JSON representation of the HTTP request.
@@ -23,12 +23,12 @@ def lambda_handler(event, context):
     """
     try:
         print("**STARTING**")
-        print("**Lambda: Authentication**")
+        print("**Lambda: Create Budget Category**")
 
         #
         # Setup AWS based on config file.
         #
-        config_file = "lambda-config.ini"
+        config_file = "config.ini"
         os.environ["AWS_SHARED_CREDENTIALS_FILE"] = config_file
 
         configur = ConfigParser()
@@ -54,54 +54,53 @@ def lambda_handler(event, context):
 
         body = json.loads(event["body"])
 
-        if "username" not in body or "password" not in body:
-            return api_utils.error(400, "missing credentials in body")
+        if "name" not in body or "budget" not in body:
+            return api_utils.error(400, "missing name or budget")
 
-        username = body["username"]
-        password = body["password"]
+        if "headers" not in event:
+            return api_utils.error(400, "no headers in request")
+
+        headers = event["headers"]
+        token: str = auth.get_token_from_header(headers)  # type: ignore
+        if token is None:
+            api_utils.error(401, "no bearer token in headers")
+
+        try:
+            userid = auth.get_user_from_token(token, secret)
+        except Exception as e:
+            return api_utils.error(401, "invalid access token: " + token)
+
+        name = body["name"]
+        budget = body["budget"]
 
         #
         # Open connection to the database.
         #
         print("**Opening connection**")
-
         db_conn = datatier.get_db_conn(
             rds_endpoint, rds_portnum, rds_username, rds_pwd, rds_dbname
         )
 
-        sql = """
-        SELECT *
-        FROM users
-        WHERE username = %s;
-        """
-        row = datatier.retrieve_one_row(db_conn, sql, [username])
-        if row == ():
+        print("**Checking if userid is valid**")
+        sql = "SELECT * FROM users WHERE userid = %s;"
+        row = datatier.retrieve_one_row(db_conn, sql, [userid])
+
+        if row == ():  # no such user
+            print("**No such user, returning...**")
             return api_utils.error(404, "no such user")
 
         sql = """
-        SELECT pwdhash
-        FROM users
-        WHERE username = %s;
+        INSERT INTO categories (category, userid, totalbudget, spent)
+        VALUES (%s, %s, %s, %s)
         """
-        pwdhash = datatier.retrieve_one_row(db_conn, sql, [username])[0]
-        if not auth.check_password(password, pwdhash):
-            return api_utils.error(401, "password incorrect")
 
-        sql = """
-        SELECT userid
-        FROM users
-        WHERE username = %s;
-        """
-        row = datatier.retrieve_one_row(db_conn, sql, [username])
-        userid = row[0]
-        token = auth.generate_token(userid, secret)
+        datatier.perform_action(db_conn, sql, [name, userid, budget, 0])
 
         #
         # Respond in an HTTP-like way, i.e. with a status
         # code and body in JSON format.
         #
         print("**DONE, returning token**")
-
         return api_utils.success(200, {"access_token": token})
 
     except Exception as err:
